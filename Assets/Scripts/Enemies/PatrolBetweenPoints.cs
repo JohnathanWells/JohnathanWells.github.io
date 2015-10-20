d89120534fa37925bef9fd0306bd3b90d5139801
Kamil Jarosz <kjarosz19946@gmail.com>
2015-10-20 12:55:56 -0400

    Separate turtle platform into its own prefab. Move patroling bodies using physics instead of translating manually.

--- Assets/Scripts/Enemies/PatrolBetweenPoints.cs
+++ Assets/Scripts/Enemies/PatrolBetweenPoints.cs
@@ -1,34 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Rigidbody2D))]
 public class PatrolBetweenPoints : MonoBehaviour {
     public float xMin;
     public float xMax;
 
     public float patrolSpeed;
 
+    private Rigidbody2D body;
+    private Vector2 velocity;
     bool headingRight;
 
 	void Start () 
     {
+        velocity = new Vector2(patrolSpeed, 0);
+        body = GetComponent<Rigidbody2D>();
+        body.isKinematic = true;
         headingRight = Random.value > 0.5f;
         Flip();
         Vector3 pos = transform.localPosition;
         pos.x = (xMax + xMin) / 2.0f;
         transform.localPosition = pos;
 	}
-	
-	void Update () 
+
+    void FixedUpdate()
     {
         if((headingRight && transform.localPosition.x > xMax) 
         || (!headingRight && transform.localPosition.x < xMin)) 
-        {
             Flip();
-        }
 
-        // Translating relative to current transform makes sure that creature is always walking in the way it's rotated.
-        transform.Translate(Vector2.right * patrolSpeed * Time.deltaTime, transform);
-	}
+        body.velocity = transform.rotation*velocity;
+    }
 
     void Flip()
     {
